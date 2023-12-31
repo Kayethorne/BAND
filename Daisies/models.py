c02a0da3ab5ecabce4d5db5092c9bbfb96d9d91e
kaylaet <kaylathorne52@gmail.com>
2023-12-31 20:56:22 +0200

    Added requirements.txt

--- Daisies/models.py
+++ Daisies/models.py
@@ -4,30 +4,48 @@ from django.contrib.auth.models import User # Import Django's User model
 # Create your models here.
 # Model for Band Members
 class BandMember(models.Model):
+    """
+    Model representing a band member.
+    """
     name = models.CharField(max_length=100)
     role_band = models.CharField(max_length=100)
     bio = models.TextField()
     image = models.ImageField(upload_to='band_members/', default='BAND/static/images/', blank=True, null=True)
 
     def __str__(self):
+        """
+        String representation of the band member.
+        """
         return self.name
     
 # Model for Albums
 class Album(models.Model):
+    """
+    Model representing an album.
+    """
     title = models.CharField(max_length=200)
     genre = models.CharField(max_length=100)
     release_date = models.DateField()
     cover_image = models.ImageField(upload_to='album/')
 
     def __str__(self):
+        """
+        String representaion of the album.
+        """
         return self.title
     
 # Model for songs
 class Song(models.Model):
+    """
+    Model representing a song.
+    """
     title = models.CharField(max_length=200)
     length = models.DurationField()
 
     def __str__(self):
+        """
+        String representation of the song.
+        """
         return self.title
 
 
